60dea901863bb98a6ca25fe9c3a60827e46766fd
mowgly11 <83482871+mowgly11@users.noreply.github.com>
2023-02-12 12:31:08 +0100

    adding cooldown to name changing

--- src/endpoints/settings.ts
+++ src/endpoints/settings.ts
@@ -15,7 +15,7 @@ let options = {
             id: id
         });
 
-        if (!user) return req.logOut((err) => {
+        if (!user || user.username == undefined) return req.logOut((err) => {
             if (err) throw new Error("Error loggin out");
             return res.redirect("/login");
         });
@@ -24,7 +24,7 @@ let options = {
 
         res.render("settings.ejs", {
             auth: req.isAuthenticated(),
-            name: user.username?.name,
+            name: user.username.name,
             provider: user.authType,
             picture: user.picture
         });
@@ -35,7 +35,7 @@ let options = {
             id: id
         });
 
-        if (!user) return req.logOut((err) => {
+        if (!user || user.username == undefined) return req.logOut((err) => {
             if (err) throw new Error("Error loggin out");
             return res.redirect("/login");
         });
@@ -46,8 +46,18 @@ let options = {
 
         const username: string = req.body.newName;
 
+        const timeLeft: number = Date.now() - Number(user.username.cooldown);
+        let left = parseInt(String(-1 * timeLeft / 1000));
+
+        if(timeLeft < 0) return res.send({ error: `Your In Cooldown, Please wait for ${left}s to perform this action again.` })
         if (username === "" || !username) return res.send({ error: "You Must Specify A Username." });
         if (filter.isProfane(username)) return res.send({ error: "Please Use A Proper Language On Your Username" });
+        if (username.length > 30) return res.send({ error: "The username length should be between 4 and 30 charecters" });
+
+        user.username.name = username;
+        user.username.cooldown = Date.now() + 60000;
+
+        await user.save();
 
         res.send({ message: "Username Set Successfully to " + username });
     }
